e46b56cecd78ed6767c2859638a9155da8e1bc10
olaifa ibukun <ibukunolaifa1984@example.com>
2023-03-15 13:33:06 +0100

    create list model

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from lists.models import List
 
-# Register your models here.
+@admin.register(List)
+class ListAdmin(admin.ModelAdmin):
+    list_select_related = ['user']
+    autocomplete_fields = ['user']
+    search_fields = ['name', 'user__username', 'user__email', 'rooms__name']
+    list_display = ['name', 'user', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+    list_filter = ['created_at', 'updated_at']
+    filter_horizontal = ['rooms']
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,20 @@
+import auto_prefetch
+
 from django.db import models
 
-# Create your models here.
+from airbnb.utils.models import NamedTimeBasedModel
+
+
+class List(NamedTimeBasedModel):
+
+    user = auto_prefetch.ForeignKey(
+        'home.CustomUser',
+        on_delete=models.CASCADE
+    )
+    rooms = models.ManyToManyField(
+        'rooms.Room',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name

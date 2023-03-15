f7771682d8c82bf39284198af0de4e135b66351b
olaifa ibukun <ibukunolaifa1984@example.com>
2023-03-15 12:58:32 +0100

    create reservation model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from reservations.models import Reservation
 
-# Register your models here.
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['room', 'guest', 'check_in', 'check_out']
+    date_hierarchy = 'check_in'
+    search_fields = ['room__name', 'guest__username', 'guest__email']
+    list_select_related = ['room', 'guest']
+    autocomplete_fields = ['room', 'guest']
+    list_filter = ['check_in', 'check_out', 'created_at', 'updated_at']
--- reservations/models.py
+++ reservations/models.py
@@ -1,2 +1,26 @@
+import auto_prefetch
 from django.db import models
 
+from airbnb.utils.models import TimeBasedModel
+from airbnb.utils.choices import ReservationStatus
+
+
+class Reservation(TimeBasedModel):
+    status = models.CharField(
+        choices=ReservationStatus.choices,
+        max_length=12,
+        default=ReservationStatus.Pending
+    )
+    guest = auto_prefetch.ForeignKey(
+        'home.CustomUser',
+        on_delete=models.CASCADE
+    )
+    room = auto_prefetch.ForeignKey(
+        'rooms.Room',
+        on_delete=models.CASCADE
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f'{self.room} - {self.check_in}'
